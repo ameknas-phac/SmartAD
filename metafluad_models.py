22e52c853882765989f40db1b4c2fa382da21e5b
awieler1313 <awieler13@gmail.com>
2024-12-16 09:02:37 -0600

    Add tiny model, and remove unused models

--- metafluad_models.py
+++ metafluad_models.py
@@ -56,148 +56,7 @@ class RegressionModel0(nn.Module):
         x = self.fc(x)
         x = F.relu(x)
         return x
-
-
-
-class MetaFluAD_late_concat(nn.Module): # Late concatenation fusion model
-    def __init__(self, metadata_dim,cnn_outdim=256):
-        super(MetaFluAD_late_concat, self).__init__()
-
-        self.SE_CNN = SE_CNN(1, cnn_outdim)
-        self.transformer = TransformerModel(cnn_outdim,  # T_input_dim,
-                                            512,  # T_hidden_dim,
-                                            2,  # T_num_layers,
-                                            4,  # T_num_heads,
-                                            64  # T_output_dim
-                                            )
-        self.ResGAT1 = MultiGAT(cnn_outdim, 128 // 2, 128 // 2, 4, 2, concat='True')
-
-        self.regression1 = RegressionModel1((512 + metadata_dim)*2, 512 + metadata_dim, 1) # Add dims for metadata
-
-    def forward(self, data):
-        x = data.x
-
-        x = self.SE_CNN(x)
-
-        x_t = self.transformer(x.unsqueeze(1)).squeeze(1)
-
-        data_x = x.squeeze(1)
-
-        x_r = self.ResGAT1(data_x, data.edge_index)
-
-        x = torch.cat((x_t.squeeze(1), x_r), dim=1)
-
-        feature = x
-
-        x = torch.cat((x,data.metadata), dim = 1) #concatenate metadata
-
-        x, n = create(x, data.edge_index, data.edge_index.shape[1]) #concatenate feature vector of nodes
-
-        ypre = self.regression1(x)
-
-        return ypre , feature
     
-class MetaFluAD_early_attention(nn.Module): # Early attention fusion model
-    def __init__(self, metadata_dim, cnn_outdim = 256):
-        super(MetaFluAD_early_attention, self).__init__()
-
-        self.SE_CNN = SE_CNN(1,cnn_outdim)
-        self.transformer = TransformerModel(cnn_outdim, 512, 2, 4, 64)
-        self.GAT = MultiGAT(cnn_outdim, 128 // 2, 128 // 2, 4, 2, concat = 'True')
-        self.regression1 = RegressionModel1(512*2,512,1)
-        self.regression2 = RegressionModel0(metadata_dim, cnn_outdim)
-
-    def forward(self, data):
-        x = data.x
-
-        x = self.SE_CNN(x)
-
-        mask = self.regression2(data.metadata) # create 256 dim vector from metadata
-
-        x = torch.mul(x,mask)
-
-        x_t = self.transformer(x.unsqueeze(1)).squeeze(1)
-
-        data_x = x.squeeze(1)
-
-        x_r = self.GAT(data_x, data.edge_index)
-
-        x = torch.cat((x_t.squeeze(1), x_r), dim = 1)
-
-        feature = x
-
-        x, n = create(x, data.edge_index, data.edge_index.shape[1])
-
-        ypre = self.regression1(x)
-
-        return ypre, feature
-
-
-class MetaFluAD_metaGAT(nn.Module):
-    def __init__(self, metadata_dim, cnn_outdim = 256):
-        super(MetaFluAD_metaGAT, self).__init__()
-        self.SE_CNN = SE_CNN(1,cnn_outdim)
-        self.transformer = TransformerModel(cnn_outdim, 512, 2, 4, 64)
-        self.GAT = MultiGAT(cnn_outdim + metadata_dim, 128 // 2, 128 // 2, 4, 2, concat = 'True')
-        self.regression1 = RegressionModel1(512*2,512,1)
-
-    def forward(self, data):
-        x = data.x
-
-        x = self.SE_CNN(x)
-
-        x_t = self.transformer(x.unsqueeze(1)).squeeze(1)
-
-        # Concatenate metadata with feature vec, before entering GAT
-        data_x = torch.cat((x.squeeze(1),data.metadata), dim = 1)
-
-        x_r = self.GAT(data_x, data.edge_index)
-
-        x = torch.cat((x_t.squeeze(1), x_r), dim = 1)
-
-        feature = x
-
-        x, n = create(x, data.edge_index, data.edge_index.shape[1])
-
-        ypre = self.regression1(x)
-
-        return ypre, feature
-    
-
-class MetaFluAD_noGAT(nn.Module):
-    def __init__(self, cnn_outdim=256):
-        super(MetaFluAD_noGAT, self).__init__()
-
-        self.SE_CNN = SE_CNN(1, cnn_outdim)
-        self.transformer = TransformerModel(cnn_outdim,  # T_input_dim,
-                                            512,  # T_hidden_dim,
-                                            2,  # T_num_layers,
-                                            4,  # T_num_heads,
-                                            64  # T_output_dim
-                                            )
-        # self.ResGAT1 = MultiGAT(cnn_outdim, 128 // 2, 128 // 2, 4, 2, concat='True')
-
-        self.regression1 = RegressionModel1(512, 256, 1)
-
-    def forward(self, data):
-        x = data.x
-
-        x = self.SE_CNN(x)
-
-        x_t = self.transformer(x.unsqueeze(1)).squeeze(1)
-
-        data_x = x.squeeze(1)
-
-        x = x_t.squeeze(1)
-
-        feature = x
-        x, n = create(x, data.edge_index, data.edge_index.shape[1])
-
-        ypre = self.regression1(x)
-
-        return ypre , feature
-    
-
 
 
 class SEBlock1(nn.Module):
@@ -243,70 +102,6 @@ class CNN1(nn.Module):
         return out
 
 
-class CNN2(nn.Module):
-    def __init__(self, in_channels, out_dim):
-        super(CNN2, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=(3, 3), stride=1, padding=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.relu = nn.ReLU(inplace=True)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
-        # self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
-        # self.pool = nn.AvgPool2d(kernel_size=2, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=(3, 3), stride=1, padding=2)
-        self.bn2 = nn.BatchNorm2d(16)
-
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=2, padding=2)
-        self.bn3 = nn.BatchNorm2d(32)
-        #
-        self.conv4 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=2, padding=2)
-        self.bn4 = nn.BatchNorm2d(64)
-        # self.fc = nn.Linear(32 * 8 * 8, out_dim)
-        # self.fc = nn.Linear(32 * 42 * 14, out_dim)
-        self.fc = nn.Linear(32 * 11 * 16, out_dim)
-        # self.fc = nn.Linear(3456, out_dim)
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.pool(out)
-
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-        out = self.pool(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-        out = self.relu(out)
-        out = self.pool(out)
-
-
-
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-
-class SECNN(nn.Module):
-    def __init__(self, in_channels, out_dim, reduction_ratio=16):
-        super(SECNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1)
-        self.se_block = SEBlock1(32, reduction_ratio)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu = self.relu = nn.ReLU(inplace=True)
-        self.cnn = CNN2(32, out_dim)
-
-    def forward(self, x):
-
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        out = self.se_block(x)
-        out = self.cnn(out)
-        return out
-
 
 class SE_CNN(nn.Module):
     def __init__(self, in_channels, out_dim, reduction_ratio=16):
@@ -345,53 +140,6 @@ class SE_CNN(nn.Module):
         out = self.fc(out)
         # out = F.relu(out)
         return out
-
-
-class GCN(torch.nn.Module):
-    def __init__(self, in_channel, hidden_channels, out_channel):
-        super(GCN, self).__init__()
-        self.conv1 = GCNConv(in_channel, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channel)
-        self.fc = nn.Linear(in_channel, out_channel)
-        self.dropout = nn.Dropout(p=0.2)
-
-    def forward(self, x, edge_index):
-        res = self.fc(x)
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.conv2(x, edge_index)
-        return F.relu(x+res)
-
-class RESGATv2(nn.Module):
-    def __init__(self, in_features, hidden_features, out_features, num_layers, num_heads, dropout=0.3):
-        super(RESGATv2, self).__init__()
-        self.dropout = dropout
-
-        self.gat_layers = nn.ModuleList()
-
-        self.gat_layers.append(GATv2Conv(in_features, hidden_features, heads=num_heads[0]))
-
-        for l in range(1, num_layers - 1):
-            self.gat_layers.append(GATv2Conv(hidden_features * num_heads[l - 1], hidden_features, heads=num_heads[l]))
-
-        self.gat_layers.append(GATv2Conv(hidden_features * num_heads[-2], out_features, heads=num_heads[-1]))
-
-        self.residual_layer = nn.Linear(in_features, out_features * num_heads[-1])
-
-    def forward(self, x, edge_index):
-        # x = data.x
-        x_res = self.residual_layer(x)
-        for layer in self.gat_layers[:-1]:
-            x = layer(x, edge_index).flatten(1)
-            x = F.elu(x)
-            x = F.dropout(x, p=self.dropout, training=self.training)
-
-        x = self.gat_layers[-1](x, edge_index).flatten(1)
-        x = F.elu(x)
-
-        x = F.relu(x + x_res)
-        return x
 
 
 class MultiGAT(torch.nn.Module):
@@ -515,24 +263,6 @@ class TransformerModel(nn.Module):
         return x
 
 
-class SEBlock(nn.Module):
-    def __init__(self, in_channels, reduction_ratio=16):
-        super(SEBlock, self).__init__()
-        self.squeeze = nn.AdaptiveAvgPool2d(1)
-        self.excitation = nn.Sequential(
-            nn.Linear(in_channels, in_channels // reduction_ratio),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_channels // reduction_ratio, in_channels),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        batch_size, channels, _, _ = x.size()
-        se = self.squeeze(x).view(batch_size, channels)
-        se = self.excitation(se).view(batch_size, channels, 1, 1)
-        return x * se
-
-
 def create(x, edge_index, num_edge):
     features = []
     for i in range(num_edge):
@@ -551,3 +281,96 @@ def create(x, edge_index, num_edge):
         # a = torch.cat(a, feature.unsqueeze(0), dim=0)
 
     return torch.stack(features), features
+
+
+class Smart_tiny(nn.Module):
+    """
+    Tiny version of the MetaFluAD model, the transformer and GAT are removed, and the output
+    dimension of the CNN is much smaller.
+    """
+    def __init__(self, cnn_outdim=32):
+        super(Smart_tiny, self).__init__()
+
+        self.SE_CNN = CNN_tiny(1, cnn_outdim)
+        self.regression1 = RegressionModel1(cnn_outdim, cnn_outdim//2, 1)
+        self.dropout1 = nn.Dropout(p=0.5)
+
+    def forward(self, data):
+        x = data.x
+        x = self.SE_CNN(x, data.edge_index, data.edge_index.shape[1])
+        x = F.leaky_relu(x, negative_slope=0.1)
+        feature = x
+        x = self.dropout1(x)
+        ypre =self.regression1(x)
+        return ypre , feature
+    
+
+class CNN_tiny(nn.Module):
+    def __init__(self, in_channels, out_dim, reduction_ratio=16):
+        super(CNN_tiny, self).__init__()
+        self.se_block2 = SEBlock1(4, reduction_ratio)
+        self.se_block3 = SEBlock1(8, reduction_ratio)
+
+        self.conv1 = CNN_custom(in_channels,4,3,1,2,1,2)
+        self.conv1b = CNN_custom(in_channels,4,3,1,2,1,2)
+        self.conv2 = CNN_custom(4,4,3,1,2,1,2)
+        self.conv2b = CNN_custom(4,4,3,1,2,1,2)
+
+        self.conv3 = CNN_custom(4,4,3,1,2,1,2)
+        self.conv4 = CNN_custom(4,8,3,1,2,1,2)
+        self.conv5 = CNN_custom(8,8,3,1,2,1,2)
+        self.conv6 = CNN_custom(8,8,3,1,3,1,3)
+
+        self.fc = nn.Linear(64, out_dim)
+
+    def forward(self, x, e, s):
+        x = create_cnnstack2(x, e, s)
+
+        x1 = self.conv1(x[:,0].unsqueeze(1))
+        x2 = self.conv1b(x[:,1].unsqueeze(1))
+        x1 = self.conv2(x1)
+        x2 = self.conv2b(x2)
+        out = x1 - x2
+
+        out = self.conv3(out)
+        out = self.se_block2(out)
+        out = self.conv4(out)
+
+        out = self.conv5(out)
+        out = self.se_block3(out)
+        out = self.conv6(out)
+
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+    
+
+class CNN_custom(nn.Module):
+    def __init__(self, in_channels, out_channels, k_conv, pad_conv, k_pool, pad_pool, str_pool):
+        super(CNN_custom, self).__init__()
+        self.in_channel = in_channels
+        self.out_channels = out_channels
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=k_conv, stride=1, padding=pad_conv)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        self.pool = nn.MaxPool2d(kernel_size=k_pool, stride=str_pool, padding=pad_pool)
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.pool(out)
+        return out
+    
+def create_cnnstack2(x, edge_index, num_edge):
+    features = []
+    for i in range(num_edge):
+        m = edge_index[0][i]
+        n = edge_index[1][i]
+        feature = torch.cat((x[m] ,x[n]))
+        features.append(feature)
+
+    return torch.stack(features)
